127d3c5aa6acb983b2224f367aff785715e892f5
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-08-01 18:27:08 +0300

    pagination logic

--- src/componenets/projects/Projects.tsx
+++ src/componenets/projects/Projects.tsx
@@ -1,18 +1,30 @@
+import { useState } from "react"
 import styles from "./projects.module.css"
 import PvPScalpel from "./PvP-Scalpel/PvPScalpel"
 import ShopifyWidget from "./shopify-widget/ShopifyWidget"
-export default function Projects() {
 
+export default function Projects() {
     const projectsArr = [
         ShopifyWidget,
         PvPScalpel
     ]
 
+    const [viewIndex, setViewIndex] = useState(0)
+    const ComponentInView = projectsArr[viewIndex]
+
     return (
         <div className={styles.projects}>
             <h2>Projects</h2>
 
-            {projectsArr && projectsArr.map((Component, index) => (<Component key={index} />))}
+            <div>
+                {projectsArr.map((_, index) => (
+                    <button key={index} onClick={() => setViewIndex(index)}>
+                        Show project {index + 1}
+                    </button>
+                ))}
+            </div>
+
+            <ComponentInView />
         </div>
     )
 }

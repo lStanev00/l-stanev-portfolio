78fe472a6b21212288651bd380b352cd0012c885
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-08-22 01:55:05 +0300

    logic to swith the view

--- src/componenets/core-wrapper/CoreWrapper.tsx
+++ src/componenets/core-wrapper/CoreWrapper.tsx
@@ -1,21 +1,31 @@
-import { ReactNode } from "react"
+import {useState } from "react"
 import styles from "./CoreWrapper.module.css"
+import Projects from "../projects/Projects";
+import Qualifications from "../qualifications/Qualifications";
+export default function CoreWrapper() {
 
-type Props = {
-    children: ReactNode,
-    components : string[]
-}
+    const ChildrenArr= [
+        Projects,
+        Qualifications
+    ]
 
-export default function CoreWrapper({children, components}:Props) {
+    const [viewIndex, setViewIndex] = useState<number>(0);
+    const ComponentInView = ChildrenArr[viewIndex]
 
     return(
     <section className={styles.mainSection}>
 
-        {components && components.map(elementName => (<button>{elementName}</button>))}
+        {ChildrenArr && ChildrenArr.map((Element, i) => (
+            <button 
+                key={Element.getName() + i}
+                onClick={() => setViewIndex(i)}
+            >
+                    {Element.getName()}
+            </button>
+            ))}
 
+        <ComponentInView />
+    </section>
+    )
 
-
-        {children}
-    </section>)
-
-}
+}   

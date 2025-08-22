4ba2ee3b46fa9d730b679ed9b1743e58cb99741f
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-08-22 16:13:33 +0300

    the contract section goes in outer fn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from './App.module.css';
 import Banner from './componenets/banner/Banner';
+import ContectMe from './componenets/contact/ContactMe';
 import CoreWrapper from './componenets/core-wrapper/CoreWrapper';
 
 function App() {
@@ -7,14 +8,7 @@ function App() {
         <main className={styles.main}>
             <Banner />
             <CoreWrapper />
-
-            <section className={styles.section}>
-                <h2>Contact</h2>
-                <p>
-                    Email: <a href="mailto:l.stanev2000@gmail.com">l.stanev2000@gmail.com</a>
-                </p>
-
-            </section>
+            <ContectMe />
         </main>
     );
 }
